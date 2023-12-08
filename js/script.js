29816bf8d27b0b956489c5743b80fe664957f5cc
AleefCma <alefedonato777hotmail.com@gmail.com>
2023-12-08 19:07:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,63 +5,93 @@ const ctx = canvas.getContext("2d")
 const size = 30
 
 const snake = [
-    {x : 200 , y : 200},
-    {x : 230 , y : 200}
+    { x: 0, y: 0 },
+    { x: 0, y: 30 }
 ]
 
-let direction,loopId
+let direction, loopId
 
 
-const drawSnake = () =>{
+const drawSnake = () => {
 
     ctx.fillStyle = "#ddd"
-    
-    snake.forEach((position,index)=>{
-        if(index===snake.length -1){
+
+    snake.forEach((position, index) => {
+        if (index === snake.length - 1) {
             ctx.fillStyle = "white"
         }
-        ctx.fillRect(position.x , position.y , size , size)
+        ctx.fillRect(position.x, position.y, size, size)
     })
 }
 
-const moveSnake = ()=>{
-    if(!direction)return
+const moveSnake = () => {
+    if (!direction) return
 
-    const head = snake[snake.length -1]
-    
+    const head = snake[snake.length - 1]
 
-    if(direction == "right"){
-        snake.push({x:head.x + size , y:head.y})
-        
+
+    if (direction == "right") {
+        snake.push({ x: head.x + size, y: head.y })
+
     }
-    if(direction == "left"){
-        snake.push({x:head.x - size , y:head.y})
-        
+    if (direction == "left") {
+        snake.push({ x: head.x - size, y: head.y })
+
     }
-    if(direction == "down"){
-        snake.push({x:head.x , y:head.y  + size})
-        
+    if (direction == "down") {
+        snake.push({ x: head.x, y: head.y + size })
+
     }
-    if(direction == "up"){
-        snake.push({x:head.x  , y:head.y - size})
-        
+    if (direction == "up") {
+        snake.push({ x: head.x, y: head.y - size })
+
     }
 
     snake.shift()
 }
+const drawGrid = () => {
 
-const gameLoop = () =>{
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "white"
+
+    ctx.lineTo(300, 0)
+    ctx.lineTo(300, 600)
+
+    ctx.stroke()
+
+}
+
+drawGrid()
+
+const gameLoop = () => {
 
     clearInterval(loopId)
 
-    ctx.clearRect(0,0,600,600)
+    ctx.clearRect(0, 0, 600, 600)
 
     moveSnake()
     drawSnake()
 
-    loopId = setTimeout(()=>{
+    loopId = setTimeout(() => {
         gameLoop()
-    },300)
+    }, 300)
 }
 
 gameLoop()
+document.addEventListener("keydown", ({ key }) => {
+
+    if (key == "ArrowRight" && direction != "left") {
+        direction = "right"
+
+    } else if (key == "ArrowUp" && direction != "down") {
+        direction = "up"      
+
+    } else if (key == "ArrowDown" && direction != "up") {
+        direction = "down"
+
+    } else if (key == "ArrowLeft" && direction!="right") {
+        direction = "left"
+
+    }
+    console.log(key)
+})
